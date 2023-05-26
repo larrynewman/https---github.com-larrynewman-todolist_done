4894ecd90e884feca4065a21b00866164d364071
larrynewman <newmanokc@gmail.com>
2023-05-26 01:47:36 -0500

    Update files.

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 //jshint esversion:6
 
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyByNJwf00memgOq_9SnQ_ZBc0Ns8X16NBc",
+//   authDomain: "todolist-373214.firebaseapp.com",
+//   projectId: "todolist-373214",
+//   storageBucket: "todolist-373214.appspot.com",
+//   messagingSenderId: "1078033490657",
+//   appId: "1:1078033490657:web:0ef05e11798973adf04da0",
+//   measurementId: "G-304F6D75K8"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
 const express = require('express');
 // const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
@@ -148,6 +170,19 @@ app.get('/about', function (req, res) {
 	res.render('about');
 });
 
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+// 	port = 3000;
+// }
+
+
+// Use this app.listen only for localhost
 app.listen(3000, function () {
 	console.log('Server started on port 3000');
+});
+
+// Google Cloud Deployment requires this port constant...
+const port = parseInt(process.env.PORT) || 8080;
+app.listen(port, () => {
+  console.log(`todolist_done: listening on port ${port}`);
 });
